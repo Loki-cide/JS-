09977d8e3938b7180b1cf79468f28a7b950d8181
Loki-cide <stharohit1245@gmail.com>
2026-06-15 22:43:26 +0545

    second commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirmPassword');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function (e) {
     e.preventDefault();
     validateForm();
 });
@@ -15,11 +15,37 @@ function validateForm() {
     const passwordValue = password.value.trim();
     const confirmPasswordValue = confirmPassword.value.trim();
 
+    console.log("", usernameValue);
+    console.log("", emailValue);
+    console.log("", passwordValue);
+    console.log("", confirmPasswordValue);
+
     if (usernameValue === '') {
         showError(username, 'Username is required');
-    } 
+    }
     else {
         showSuccess(username);
+    }
+
+    if (emailValue === '') {
+        showError(email, 'Email is required');
+    }
+    else {
+        showSuccess(email);
+    }
+
+    if (passwordValue === '') {
+        showError(password, 'Password is required');
+    }
+    else {
+        showSuccess(password);
+    }
+
+    if (confirmPasswordValue === '') {
+        showError(confirmPassword, 'Password is required');
+    }
+    else {
+        showSuccess(confirmPassword);
     }
 }
 
@@ -33,5 +59,5 @@ function showError(input, message) {
 
 function showSuccess(input) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control success';
+    formControl.classList.add('success');
 }
